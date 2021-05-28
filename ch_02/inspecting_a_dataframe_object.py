8beb967caf653e501dd0cc8c233ac9ec3e7a72bc
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-05-28 12:28:28 -0400

    Tested Pandas on PyCharm and Changed VS Code Settings
    Experimented with displaying DataFrame objects in PyCharms.
    It didn't work.
    
    Also changed some settings for VS Code.
    Updated the JSON file associated with VS Code.
    
    Harrison Strang

--- ch_02/inspecting_a_dataframe_object.py
+++ ch_02/inspecting_a_dataframe_object.py
@@ -4,11 +4,17 @@ The :py:mod:`ch_02.inspecting_a_dataframe_object` will show me how to get basic 
 """
 from ch_02.dataframe_test_instance import df_csv
 
-var = df_csv.empty
+is_empty = df_csv.empty
 """
 bool: A Boolean value that tells me whether the DataFrame instance is empty or not
 """
 
-if __name__ == '__main__':
-    print(var)
+dimensions = df_csv.shape
+"""
+(int, int): The dimensions of my :obj:`DataFrame` instance.
+"""
 
+if __name__ == '__main__':
+    print(is_empty)
+    print(dimensions)
+    print(type(dimensions))
