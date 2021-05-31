2dd9d292bf955de7bb5cf54edd112a163f83cb7b
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-05-31 12:47:35 -0400

    Update Chapter 03 Notebook
    Added sections describing wide data and long data.
    Emphasized the differences between the two formats.
    Started to learn how to use seaborn.
    
    Harrison Strang

--- .virtual_documents/ch_03/my_ch_03_work.ipynb.py
+++ .virtual_documents/ch_03/my_ch_03_work.ipynb.py
@@ -1,6 +1,7 @@
 # Necessary imports
-from matplotlib import pyplot as plt
 import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 # File paths
 wide_data_file_path = 'data/wide_data.csv'
@@ -37,4 +38,49 @@ pandas.core.frame.DataFrame: The long format DataFrame instance.
 long_df = long_df[use_columns_long]
 
 
+# Get the first x number of columns from the wide DataFrame instance
+number_of_rows_to_get = 6
+"""
+int: The number of rows to display.
+"""
+wide_df.head(number_of_rows_to_get)
 
+wide_df.describe(include = 'all', datetime_is_numeric = True)
+
+long_df.head(number_of_rows_to_get)
+
+# Seaborn variables
+y_label = 'Temperature in Celsius'
+"""
+str: The y-axis label for this seaborn plot.
+"""
+title = 'Temperature in NYC (October 2018)'
+"""
+str: The title for the seaborn plot.
+"""
+rc_dict = {'figure.figsize': (15, 5)}
+"""
+{str: (int, int)}: A dictionary defining the seaborn figure size.
+"""
+style_str = 'white'
+"""
+str: The style argument for the seaborn plot.
+"""
+
+# Drawing the plot
+sns.set_theme(rc = rc_dict, style = style_str)
+ax = sns.lineplot(
+        data = long_df,
+        x = 'date',
+        y = 'value',
+        hue = 'datatype'
+)
+ax.set_ylabel(y_label)
+ax.set_title(title)
+plt.show()
+
+rc_dict = {'figure.figsize': (20, 10)}
+font_scl = 2
+"""
+int: The scaling for fonts in the faceted plots.
+"""
