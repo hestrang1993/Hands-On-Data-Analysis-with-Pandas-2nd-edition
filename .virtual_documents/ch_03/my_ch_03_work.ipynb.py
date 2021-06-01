63e40e8624719127965e4f60881f9a432103720a
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-05-31 21:21:50 -0400

    Updated Chapter 03 Notebook
    Began using API to pull data from the NCEI.
    Finished the first section of chapter 03.
    
    Harrison Strang

--- .virtual_documents/ch_03/my_ch_03_work.ipynb.py
+++ .virtual_documents/ch_03/my_ch_03_work.ipynb.py
@@ -1,7 +1,7 @@
 # Necessary imports
+from matplotlib import pyplot as plt
 import pandas as pd
 import seaborn as sns
-from matplotlib import pyplot as plt
 
 # File paths
 wide_data_file_path = 'data/wide_data.csv'
@@ -45,9 +45,12 @@ int: The number of rows to display.
 """
 wide_df.head(number_of_rows_to_get)
 
+
 wide_df.describe(include = 'all', datetime_is_numeric = True)
 
+
 long_df.head(number_of_rows_to_get)
+
 
 # Seaborn variables
 y_label = 'Temperature in Celsius'
@@ -70,17 +73,53 @@ str: The style argument for the seaborn plot.
 # Drawing the plot
 sns.set_theme(rc = rc_dict, style = style_str)
 ax = sns.lineplot(
-        data = long_df,
-        x = 'date',
-        y = 'value',
-        hue = 'datatype'
+    data = long_df,
+    x = 'date',
+    y = 'value',
+    hue = 'datatype'
 )
 ax.set_ylabel(y_label)
 ax.set_title(title)
 plt.show()
 
+
+# Plot variables
 rc_dict = {'figure.figsize': (20, 10)}
+"""
+{str: (int, int)}: A dictionary defining the seaborn figure size.
+"""
 font_scl = 2
 """
 int: The scaling for fonts in the faceted plots.
+"""
+title_size = 25
+"""
+str: The size of the title for the facet plots.
+"""
+x_label_rotation = 90
+"""
+int: The degree of rotation for the x-labels.
+"""
+plot_height = 10
+"""
+int: The height of the faceted plots.
+"""
+col_key = 'datatype'
+"""
+str: The key to facet out the datatype.
+"""
+
+# Drawing the plots
+sns.set(rc = rc_dict, style = style_str, font_scale = font_scl)
+g = sns.FacetGrid(long_df, col = col_key, height = plot_height)
+g = g.map(plt.plot, 'date', 'value')
+g.set_titles(size = title_size)
+g.set_xticklabels(rotation = x_label_rotation)
+plt.show()
+
+
+# The token dictionary I need to access the NCEI data.
+api_token_dict = {'token': 'ysVIqRTQlsItJQPLcOelgYbIIxPancXx'}
+"""
+{str: str}: The NCEI API token key I need to access the data.
 """
