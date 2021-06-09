074eb7895d3e6c2c6bb330b5c09ab3aba81128eb
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-06-08 21:11:03 -0400

    Updated My Chapter 03 Notebook
    Added helper functions to look for NY information.
    Requested data from Central Park.
    
    Harrison Strang

--- .virtual_documents/ch_03/my_ch_03_work.ipynb.py
+++ .virtual_documents/ch_03/my_ch_03_work.ipynb.py
@@ -320,4 +320,312 @@ payload = response.json()
 pprint(payload)
 
 
+def get_data_midpoint(start = 1, end = None):
+    """
+    Get the midpoint of the data set.
+
+    I will use this midpoint to cut the data in half after each search.
+
+    Parameters
+    ----------
+    start : int, optional
+        The index to start the search at.
+
+        I don't need to touch this.
+        The functions I plan to make will update this through recursion.
+    end : int or None, optional
+        The last index.
+
+        I use this to find the midpoint.
+        The functions I plan to make will update this through recursion.
+
+    Returns
+    -------
+    int
+        The midpoint for the dataset.
+    """
+    mid = (start + (end or 1)) // 2
+    return mid
+
+
+def to_lower(name):
+    """
+    Make a name lowercase.
+
+    I make the name lowercase so any search won't run into case-sensitivity issues.
+
+    Parameters
+    ----------
+    name : str
+        The name of the item I want to look for.
+
+    Returns
+    -------
+    str
+        The name of the item I want to look for in lowercase font.
+    """
+    name = name.lower()
+    return name
+
+
+def define_payload(mid):
+    """
+    Define the payload to send with each request.
+
+    Parameters
+    ----------
+    mid : int
+        The midpoint of the dataset I want to search through.
+
+    Returns
+    -------
+    dict[str: str, str: str, str: int, str: int]
+        The payload I want to get with each NCEI API request.
+    """
+    payload_to_get = {
+            "datasetid": "GHCND",
+            "sortfield": "name",
+            "offset": mid,
+            "limit": 1
+    }
+    return payload_to_get
+
+
+def get_response_out(what, endpoint_in, start = 1, end = None):
+    """
+    Get the NCEI GET request instance.
+
+    Parameters
+    ----------
+    what : dict[str, str]
+        A dictionary specifying
+    endpoint_in : str
+        The name of the location I want to look in.
+    start : int, optional
+        The position to start looking at.
+    end : int or None, optional
+        The last position to look at.
+
+    Returns
+    -------
+    requests.Response
+        The API request in a JSON file format.
+    """
+    mid = get_data_midpoint(start, end)
+    payload_to_get = define_payload(mid)
+    response_out = make_request(endpoint_in, {**payload_to_get, **what})
+    return response_out
+
+
+def get_payload_out(response_to_convert):
+    """
+    Get a dictionary version of the JSON response.
+
+    Parameters
+    ----------
+    response_to_convert : requests.Response
+        The GET request I want to convert into a dictionary.
+
+    Returns
+    -------
+    dict
+        A dictionary version of the JSON response I want.
+    """
+    payload_out = response_to_convert.json()
+    return payload_out
+
+
+def get_the_end_index(end, payload_in):
+    """
+    Return the end index
+
+    Parameters
+    ----------
+    end : int
+        The old end index.
+    payload_in : dict
+        A JSON Response instance that has been converted into a dictionary.
+
+    Returns
+    -------
+    int
+        The new end index to use.
+    """
+    m = 'metadata'
+    r = 'resultset'
+    c = 'count'
+    end_index = end or payload_in[m][r][c]
+    return end_index
+
+
+def get_current_name(payload_in):
+    """
+    Grab the lowercase version of the current name.
+
+    Parameters
+    ----------
+    payload_in : dict
+        A JSON Response instance that has been converted into a dictionary.
+
+    Returns
+    -------
+    str
+        The lowercase version of the string found at a particular index in the dictionary I loaded in.
+    """
+    r = 'results'
+    n = 'name'
+    index = 0
+    current_name = payload_in[r][index][n]
+    current_name = to_lower(current_name)
+    return current_name
+
+
+def get_item(name, what, endpoint, start = 1, end = None):
+    """
+    Grab the JSON payload for a given field by name using binary search.
+
+    Parameters
+    ----------
+    name : str
+        The item to look for.
+    what: dict[str, str]
+        Dictionary specifying what the item in ``name`` is.
+    endpoint : str
+        Where to look for the item.
+    start : int, optional
+        The position to start at.
+
+        We don't need to touch this, but the function will manipulate this with recursion.
+    end : int or None, optional
+        The last position of the items.
+
+        Used to find the midpoint, but like ``start`` this is not something we need to worry about.
+
+    Returns
+    -------
+    dict
+        Dictionary of the information for the item if found otherwise
+        an empty dictionary.
+    """
+    # Get the midpoint of the data set.
+    mid = get_data_midpoint(start, end)
+
+    # Make our request adding any additional filter parameters from ``what``.
+    response_to_get = get_response_out(what, endpoint, start, end)
+
+    # Make the name lowercase.
+    name = to_lower(name)
+
+    # Check the response, if it went through.
+    if response.ok:
+
+        # Convert the JSON response into a dictionary.
+        payload_i_got = response_to_get.json()
+
+        # Find the end index of the dictionary.
+        end = get_the_end_index(end, payload_i_got)
+
+        # Get the string item at the end of the dictionary.
+        current_name = get_current_name(payload_i_got)
+
+        # Return the found item.
+        if name in current_name:
+            return payload_i_got['results'][0]
+        else:
+            # If our start index is greater than or equal to our end, we couldn't find it
+            if start >= end:
+                return {}
+            # Our name comes before the current name in the alphabet, so we search further to the left
+            elif name < current_name:
+                return get_item(name, what, endpoint, start, mid - 1)
+            # Our name comes after the current name in the alphabet, so we search further to the right
+            elif name > current_name:
+                return get_item(name, what, endpoint, mid + 1, end)
+    else:
+        # The response wasn't ok, use code to determine why
+        print(f'Response not OK, status: {response.status_code}')
+
+
+# Get NYC's ID
+nyc_name = "New York"
+"""
+str: The item to look for.
+
+In this case, it will be NYC.
+"""
+nyc_what = {"locationcategoryid": "CITY"}
+"""
+dict[str: str]: Dictionary specifying what the item in ``nyc_name`` is.
+"""
+nyc_endpoint = "locations"
+"""
+str: Where to look for the NYC ID.
+"""
+nyc = get_item(nyc_name, nyc_what, nyc_endpoint)
+nyc
+
+
+central_park_name = 'NY City Central Park'
+"""
+str: The item to look for.
+
+In this case, it is the station at NYC Central Park.
+"""
+central_park_what = {
+    'locationid': nyc['id']
+}
+"""
+dict[str: dict]: Dictionary specifying what the item in ``central_park_name`` is.
+"""
+central_park_endpoint = 'stations'
+"""
+str: Where to look for the NYC Central Park ID.
+"""
+central_park = get_item(central_park_name, central_park_what, central_park_endpoint)
+central_park
+
+
+central_park_endpoint = 'data'
+"""
+str: The endpoint for where I the temperature data for NYC Central Park is.
+"""
+central_park_payload_in = {
+        'datasetid': 'GHCND',
+        'stationid': central_park['id'],
+        'locationid': nyc['id'],
+        'startdate': '2018-10-01',
+        'enddate': '2018-10-31',
+        'datatypeid': [
+                'TAVG',
+                'TMAX',
+                'TMIN'
+        ],
+        'units': 'metric',
+        'limit': 1000
+}
+"""
+dict: The payload I need to submit to get the temperature data for NYC Central Park.
+"""
+central_park_response = make_request(central_park_endpoint, central_park_payload_in)
+central_park_response.ok
+
+
+# Convert the Response instance into a dictionary
+grab = 'results'
+"""
+str: The part of the Response instance to grab.
+"""
+central_park_response = central_park_response.json()[grab]
+"""
+dict: A dictionary with all the temperatures from NYC Central Park.
+"""
+
+# Create the DataFrame instance
+df_central_park = pd.DataFrame(central_park_response)
+"""
+pandas.core.frame.DataFrame: The DataFrame with the daily minimum and maximum temperatures from NYC Central Park during October, 2018.
+"""
+df_central_park.head()
+
+
 
